049274232bf5a0d171908e2bfc653be7a54a5fe0
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2023-08-08 21:14:40 +0200

    API-Question-Feature

--- controllers/question.js
+++ controllers/question.js
@@ -1,10 +1,104 @@
-const getAllQuestions= (req,res) =>{
-res.status(200)
-.json({
-    success:true
-});
-};
+const Question = require('../models/Question');
+const CustomError = require('../helpers/error/CustomError');
+const asyncHandler = require('express-async-handler');
+const question = require('../models/Question');
 
-module.exports={
-    getAllQuestions
-}
+const askNewQuestion = asyncHandler(async (req, res, next) => {
+  const { title, content } = req.body;
+  const question = await Question.create({
+    title: title,
+    content: content,
+    user: req.user.id,
+  });
+  return res.status(200).json({
+    success: true,
+    question: question,
+  });
+});
+
+const getAllQuestions = asyncHandler(async (req, res, next) => {
+  const questions = await Question.find();
+  return res.status(200).json({
+    success: true,
+    questions: questions,
+  });
+});
+
+const getSingleQuestion = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const question = await Question.findById(id);
+  if (!question) {
+    next(new CustomError('There is no such a question with this id'), 404);
+  }
+  return res.status(200).json({
+    success: true,
+    question: question,
+  });
+});
+
+const editQuestion = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const { title, content } = req.body;
+  let question = await Question.findById(id);
+  question.title = title;
+  question.content = content;
+  question = await question.save();
+
+  return res.status(200).json({
+    success: true,
+    question: question,
+  });
+});
+const deleteQuestion = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  await Question.findByIdAndDelete(id);
+
+  return res.status(200).json({
+    success: true,
+    message: 'Question is deleted',
+  });
+});
+const likeQuestion = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const question = await Question.findById(id);
+
+  if (question.likes.includes(req.user.id)) {
+    return next(new CustomError('You already liked this question', 400));
+  }
+  question.likes.push(req.user.id);
+  await question.save();
+
+  return res.status(200).json({
+    success: true,
+    data: question,
+  });
+});
+
+const undoLikeQuestion = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const question = await Question.findById(id);
+
+  if (!question.likes.includes(req.user.id)) {
+    return next(
+      new CustomError('You have not liked before this question', 400)
+    );
+  }
+  const index = question.likes.indexOf(req.user.id);
+  question.likes.splice(index, 1);
+  await question.save();
+
+  return res.status(200).json({
+    success: true,
+    data: question,
+  });
+});
+
+module.exports = {
+  askNewQuestion,
+  getAllQuestions,
+  getSingleQuestion,
+  editQuestion,
+  deleteQuestion,
+  likeQuestion,
+  undoLikeQuestion,
+};
--- middlewares/database/databaseErrorHelpers.js
+++ middlewares/database/databaseErrorHelpers.js
@@ -1,19 +1,32 @@
-const User=require("../../models/user")
-const CustomError = require('../../helpers/error/CustomError')
-const asyncHandler = require('express-async-handler')
+const User = require('../../models/User');
+const CustomError = require('../../helpers/error/CustomError');
+const asyncHandler = require('express-async-handler');
+const Question = require('../../models/Question');
 
+const checkUserExist = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
 
-const checkUserExist=asyncHandler(async (req, res, next) => {
+  const user = await User.findById(id);
 
-    const {id}=req.params;
+  if (!user) {
+    return next(new CustomError('There is no such user with this id!', 400));
+  }
+  req.dataUser = user;
+  next();
+});
 
-    const user=await User.findById(id);
+const checkQuestionExist = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
 
-    if(!user){
-        return next(new CustomError("There is no such user with this id!",400))
-    }
-    req.dataUser=user
-    next();
-})
+  const question = await Question.findById(id);
 
-module.exports={checkUserExist}
+  if (!question) {
+    return next(
+      new CustomError('There is no such question with this id!', 400)
+    );
+  }
+
+  next();
+});
+
+module.exports = { checkUserExist, checkQuestionExist };
